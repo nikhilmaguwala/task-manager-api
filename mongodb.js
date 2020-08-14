0262e58f26dc2f824b6b4a187c7ccac928541f56
Atmiya-Group-of-Education <atmiyaagroupofeducation@gmail.com>
2020-08-14 16:22:40 +0530

    Init commit

--- mongodb.js
+++ mongodb.js
@@ -11,49 +11,6 @@ MongoClient.connect(connectionURL, { useNewUrlParser : true } , (error,client) =
     }
     const db = client.db(databaseName)
 
-    // db.collection('users').insertOne({
-    //     name:'Nikhil',
-    //     age:27
-    // },(error,result) => {
-    //     if(error){
-    //         return console.log('Unable to insert user');
-    //     }
-    //     console.log(result.ops)
-    // })
-
-    // db.collection('users').insertMany([
-    //     {
-    //         name : 'Jen',
-    //         age : 28
-    //     }, {
-    //         name : 'Gunther',
-    //         age : 27
-    //     }
-    // ], (error,result) => {
-    //     if(error){
-    //         return console.log('Unabke to insert document');
-    //     }
-    //     console.log(result.ops);
-    // })
-
-    // db.collection('tasks').insertMany([
-    //     {
-    //         description : 'Clean the House',
-    //         completed : true
-    //     }, {
-    //         description : 'Renew inspection',
-    //         completed : false
-    //     }, {
-    //         description :'pot plants',
-    //         completed : false
-    //     }
-    // ], (error,result) => {
-    //     if(error){
-    //         return console.log('Unable to insert task!');
-    //     }
-    //     console.log(result.ops);
-    // })
-
 })
 
 
